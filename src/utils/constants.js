90aa40c42dc864afafc7845e069e280c88f9bb09
cheetiakanksha <cheetiakanksharao1@gmail.com>
2024-08-08 17:08:55 -0700

    chnaged key

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -10,4 +10,3 @@ export const API_OPTIONS={
   };
 export const SUPPORTED_LANGUAGES=[{identifier:"en", name:"English"},{identifier:"hindi", name:"Hindi"},{identifier:"spanish", name:"Spanish"}]
 export const IMG_CDN="https://image.tmdb.org/t/p/w500";
-export const OPENAI_KEY=process.env.REACT_APP_OPENAI_KEY;
